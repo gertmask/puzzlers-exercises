f5f29b0f63b98b0ea6fcbeed28d8ce65da9fe985
gertmask <gertmask@users.noreply.github.com>
2018-08-06 18:34:34 +0300

    - bf solution for two-sum problem

--- exercises/src/main/java/com/gertmask/Exercise1.java
+++ exercises/src/main/java/com/gertmask/Exercise1.java
@@ -1,10 +1,42 @@
 package com.gertmask;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.tuple.Pair;
+
 /**
+ * Two-Sum Problem:
+ *  Given an array of integers and an integer target, return the indices of two numbers that add up to the target.
+ *
+ *  Example:
+ *      int[] input = { 2, 4, 3, 3 }
+ *      target = 6
+ *
+ *      result: {2,4}, {3,3}
+ *
  * @author gertmask
  * @version 1.0, 03-Aug-18
  */
 public class Exercise1 {
 
-    // Constructors
+    @SuppressWarnings("unchecked")
+    private static final Pair<Integer, Integer>[] EMPTY_PAIR_ARRAY = new Pair[0];
+
+
+    public Pair<Integer, Integer>[] solutionBruteForce(int[] input, int target) {
+        List<Pair<Integer, Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < input.length; i++) {
+            for (int j = i + 1; j < input.length; j++) {
+                int left = input[i];
+                int right = input[j];
+
+                if (left + right == target) {
+                    result.add(Pair.of(left, right));
+                }
+            }
+        }
+
+        return result.toArray(EMPTY_PAIR_ARRAY);
+    }
 }
--- exercises/src/test/java/com/gertmask/Exercise1Test.java
+++ exercises/src/test/java/com/gertmask/Exercise1Test.java
@@ -1,11 +1,83 @@
 package com.gertmask;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.Before;
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
 /**
  * @author gertmask
  * @version 1.0, 03-Aug-18
  */
+@RunWith(Theories.class)
 public class Exercise1Test {
 
+    @DataPoint
+    public static final TestData TD_1 = new TestData(new int[]{2, 4, 3, 3}, 6, toPairArray(2, 4, 3, 3));
+    @DataPoint
+    public static final TestData TD_2 = new TestData(new int[]{5, 3, 7, 0, 1, 4, 2}, 5, toPairArray(5, 0, 3, 2, 1, 4));
+
+    private Exercise1 exc;
+
+    @Before
+    public void setUp() {
+        exc = new Exercise1();
+    }
+
+    @Theory
+    public void testBruteForceSolution(TestData td) {
+        Pair<Integer, Integer>[] result = exc.solutionBruteForce(td.getInputInts(), td.getTarget());
+        assertArrayEquals(td.getExpectedPais(), result);
+    }
+
+    // **************************************************************************
+    // Private methods
+    // **************************************************************************
+
+    @SuppressWarnings("unchecked")
+    private static Pair<Integer, Integer>[] toPairArray(int... items) {
+        if (items.length % 2 != 0) {
+            throw new IllegalArgumentException("Number of items should be even");
+        }
+
+        Pair<Integer, Integer>[] result = new Pair[items.length / 2];
+        for (int i = 0; i < items.length; i = i + 2) {
+            result[i / 2] = Pair.of(items[i], items[i + 1]);
+        }
+
+        return result;
+    }
+
+    // **************************************************************************
+    // Inner classes and interfaces
+    // **************************************************************************
+
+    private static class TestData {
+
+        private int[] inputInts;
+        private int target;
+        private Pair<Integer, Integer>[] expectedPais;
+
+        TestData(int[] inputInts, int target, Pair<Integer, Integer>[] expectedPais) {
+            this.inputInts = inputInts;
+            this.target = target;
+            this.expectedPais = expectedPais;
+        }
+
+        int[] getInputInts() {
+            return inputInts;
+        }
+
+        int getTarget() {
+            return target;
+        }
+
+        Pair<Integer, Integer>[] getExpectedPais() {
+            return expectedPais;
+        }
+    }
 }
